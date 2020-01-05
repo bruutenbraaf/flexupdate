b00745d23ffc913ddde3b2df8d9c1bd3c06c0aa7
Bruut & Braaf <info@bruutenbraaf.nl>
2020-01-05 12:15:48 +0100

    Fix page

--- page.php
+++ page.php
@@ -1,15 +1,5 @@
 <?php
 get_header(); ?>
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <main id="static">
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-        </main>
-    <?php endwhile; ?>
-<?php endif; ?>
-
-
 <?php
 get_header(); ?>
 <main id="static">
