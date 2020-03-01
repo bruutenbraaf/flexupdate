3ea9465271500052c2db8cbad65e500699b8ecd6
Bruut & Braaf <info@bruutenbraaf.nl>
2020-03-01 11:26:13 +0100

    Author

--- single.php
+++ single.php
@@ -24,7 +24,6 @@ get_header(); ?>
                         $a_id = $post->post_author;
                         $author = get_user_by('slug', get_query_var('author_name'));
                         $nickname = get_the_author_meta('nickname', $author->ID);  ?>
-
                         <?php if ($a_id) { ?>
                             <div class="ing">
                                 <a href="<?php echo get_author_posts_url($a_id); ?>">
