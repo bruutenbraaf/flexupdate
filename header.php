3368157acb32cca25bd22b5b4bf1a8aa6c7fc9c7
Bruut & Braaf <info@bruutenbraaf.nl>
2020-03-01 11:15:18 +0100

    Fix nav
    Hide buttons on search

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
                         </a>
                     <?php } ?>
                 </div>
-                <div class="p-2 mx d-flex align-items-center">
+                <div class="p-2 mx btnsnav">
                     <div class="nav-second-btn news-btn">Aanmelden nieuwsbrief</div>
                     <?php wp_nav_menu(array('theme_location' => 'main_menu')); ?>
                 </div>
